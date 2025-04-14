5da918c80d892bb115496ff1ba4a2d3a60dc34e5
c0del1ar <aryakrishna436@gmail.com>
2025-04-14 20:10:31 +0700

    update sc

--- src/commands/main-sc.go
+++ src/commands/main-sc.go
@@ -11,7 +11,7 @@ func init() {
 		Tags:     "main",
 		IsPrefix: true,
 		Exec: func(client *libs.NewClientImpl, m *libs.IMessage) {
-			m.Reply("https://github.com/fckvania/koenbotGo\n\n_Free Not For Sell_")
+			m.Reply("https://github.com/c0del1ar/koenbot\n\n_Origin from https://github.com/fckvania/MaoGo_\n\n_Free Not For Sell_")
 		},
 	})
 }
